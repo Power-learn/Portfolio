f05dd9c150e5c38f87d6b273601442e767931df2
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-04-11 07:28:29 +0300

    python code

---
+++ week1/user_input.py
@@ -0,0 +1,7 @@
+# Ask for user input
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where are you located? ")
+
+# Print personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
