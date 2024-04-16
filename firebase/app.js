568138b4bfcc53e14740aaadaed853ab0192c704
robert5313 <robert.kimaiyo@masterclass.co.ke>
2024-04-16 16:16:53 +0300

    added python file

---
+++ firebase/app.js
@@ -0,0 +1,19 @@
+firebase.initializeApp(
+    {
+        apiKey: "AIzaSyB-6F6kVjQwX6v7f9tG5y3LXG2Gv2Q4e1A",
+        authDomain: "todo-1f6d9.firebaseapp.com",
+        projectId: "todo-1f6d9",
+        storageBucket: "todo-1f6d9.appspot.com",
+        messagingSenderId: "1062718499434",
+        appId: "1:1062718499434:web:3c9b5f5d1f6b4c0f0e9a4e" 
+    }
+);
+
+const db = firebase.firestore();
+
+function addTask() {
+    const taskInput = d.getElementById('taskInput');
+    const task = taskInput.value.trim();
+    if(task) ==
+}
+
